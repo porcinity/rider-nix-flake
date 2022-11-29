abb71f1f628ca605b80b4f354cd98050f9defd4e
Ant <it@me.org>
2022-11-29 09:03:36 -0600

    Update flake file

--- flake.nix
+++ flake.nix
@@ -7,9 +7,13 @@
       url = "github:numtide/flake-utils";
     };
 
-    nixpkgs = {
-      url = "nixpkgs/nixos-22.05";
-    };
+    nixpkgs.url = "nixpkgs/nixos-22.11";
+
+    # 22.05 works:  
+    # nixpkgs.url = "nixpkgs/nixos-22.05";
+
+    # Unstable doesn't work
+    # nixpkgs.url = "nixpkgs/nixos-unstable"
 
   };
 
